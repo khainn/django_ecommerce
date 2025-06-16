7a4dbb8c594c8af6180f88338ab2ecac6a9daa15
khainn <ngockhaibk@gmail.com>
2025-06-16 09:29:54 +0700

    fix: Update locale BaseModel

--- common/models.py
+++ common/models.py
@@ -1,12 +1,13 @@
 import uuid
 
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created At"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Updated At"))
 
     class Meta:
         abstract = True
