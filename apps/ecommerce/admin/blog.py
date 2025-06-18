406008244f9d0821b252b3008acd74539bf9d48c
khainn <ngockhaibk@gmail.com>
2025-06-18 16:40:47 +0700

    feat: Update Blog and Blog Image admin site

--- apps/ecommerce/admin/blog.py
+++ apps/ecommerce/admin/blog.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib import admin
 
 from apps.ecommerce.models import Blog, BlogImage
-from apps.ecommerce.widgets import NoCurrentFileClearableFileInput
 from .common import BaseModelAdmin, ImagePreviewMixin
 from django.utils.translation import gettext_lazy as _
 
@@ -12,7 +11,6 @@ class BlogImageInline(admin.TabularInline, ImagePreviewMixin):
     extra = 1
     fields = ("image", "image_preview", "caption", "display_order")
     readonly_fields = ("image_preview",)
-    classes = ('collapse',)
 
 
 class BlogAdminForm(forms.ModelForm):
@@ -20,7 +18,6 @@ class BlogAdminForm(forms.ModelForm):
         model = Blog
         fields = "__all__"
         widgets = {
-            'image': NoCurrentFileClearableFileInput,
             'title': forms.TextInput(attrs={'class': 'vTextField', 'style': 'width: 100%;'}),
             'content': forms.Textarea(attrs={'rows': 10, 'cols': 80}),
         }
@@ -39,25 +36,3 @@ class BlogAdmin(BaseModelAdmin):
 
     def get_queryset(self, request):
         return super().get_queryset(request)
-
-
-class BlogImageAdminForm(forms.ModelForm):
-    class Meta:
-        model = BlogImage
-        fields = "__all__"
-        widgets = {
-            'image': NoCurrentFileClearableFileInput,
-        }
-
-
-class BlogImageAdmin(BaseModelAdmin):
-    list_display = ("short_id", "blog", "caption", "display_order", "created_at")
-    list_filter = ("blog", "created_at")
-    search_fields = ("blog__title", "caption")
-    readonly_fields = ("image_preview",)
-    fields = ("blog", "image_preview", "image", "caption", "display_order")
-    form = BlogImageAdminForm
-    list_select_related = ("blog",)
-
-    def get_queryset(self, request):
-        return super().get_queryset(request).select_related('blog')
